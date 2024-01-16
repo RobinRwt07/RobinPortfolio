4c14956fb21b5bb6bb326e103371135a95163d6f
Robin Rawat <robinrwt85@gmail.com>
2024-01-16 23:35:47 +0530

    Add dark mode feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*/*.{html,js}"],
+  content: ["./index.html",'./**/*.{html,js}'],
   theme: {
     extend: {},
     colors:{
-      darkBlue:"#111E34",
+      darkBlue:"#041636",
       darkVoid:"#151419",
       DustyGrey:"#878787",
       lightGrey:"#F9F9F9",
-      gluonGrey:"#1B1B1E",
       snow:"#FBFBFB",
-      lava:"#F56E0F",
-      slateGret:"#262626",
+      lava:"#f5860f",
       teal:"#003049",
       grey:"#E0E0E0",
       transparent:"transparent",
+    
     },
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
     },
   },
   plugins: [],
+  darkMode:'class',
 }
 
